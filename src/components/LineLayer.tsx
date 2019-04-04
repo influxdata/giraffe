6bb0aa29e01d92f525dd4620cbc2a2e1921ab509
Christopher Henn <chris@chrishenn.net>
2019-04-04 16:36:16 -0700

    Memoize line layer computation

--- src/components/LineLayer.tsx
+++ src/components/LineLayer.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import {useRef, useLayoutEffect, FunctionComponent} from 'react'
+import {useRef, useLayoutEffect, useMemo, FunctionComponent} from 'react'
 import {line, curveLinear} from 'd3-shape'
 import {range} from 'd3-array'
 
@@ -9,27 +9,59 @@ import {clearCanvas} from '../utils/clearCanvas'
 import {GROUP_COL_KEY, CURVES} from '../constants'
 import {isDefined} from '../utils/isDefined'
 
+interface LineDatum {
+  xs: number[]
+  ys: number[]
+  fill: string
+}
+
+const computeLineData = (
+  table: Table,
+  xColKey: string,
+  yColKey: string,
+  fillScale: Scale<string, string>
+): LineDatum[] => {
+  const xCol = table.columns[xColKey].data
+  const yCol = table.columns[yColKey].data
+  const groupCol = table.columns[GROUP_COL_KEY].data as string[]
+  const groupKeys = new Set(groupCol)
+
+  const result = []
+
+  for (let groupKey of groupKeys) {
+    let xs = []
+    let ys = []
+
+    for (let i = 0; i < table.length; i++) {
+      if (groupCol[i] !== groupKey) {
+        continue
+      }
+
+      xs.push(xCol[i])
+      ys.push(yCol[i])
+    }
+
+    result.push({xs, ys, fill: fillScale(groupKey)})
+  }
+
+  return result
+}
+
 interface DrawLinesOptions {
   canvas: HTMLCanvasElement
-  table: Table
-  xColKey: string
-  yColKey: string
   xScale: Scale<number, number>
   yScale: Scale<number, number>
-  fillScale: Scale<string, string>
   interpolation: LineInterpolation
+  lineData: LineDatum[]
   width: number
   height: number
 }
 
 const drawLines = ({
   canvas,
-  table,
-  xColKey,
-  yColKey,
   xScale,
   yScale,
-  fillScale,
+  lineData,
   interpolation,
   width,
   height,
@@ -38,25 +70,7 @@ const drawLines = ({
 
   const context = canvas.getContext('2d')
 
-  const xCol = table.columns[xColKey].data
-  const yCol = table.columns[yColKey].data
-  const groupCol = table.columns[GROUP_COL_KEY].data as string[]
-
-  const groupKeys = new Set(groupCol)
-
-  for (const groupKey of groupKeys) {
-    let xs = []
-    let ys = []
-
-    for (let i = 0; i < table.length; i++) {
-      // TODO: This can be done once when the config for the `PlotEnv` is set,
-      // rather than every time the `LineLayer` renders
-      if (groupCol[i] === groupKey) {
-        xs.push(xCol[i])
-        ys.push(yCol[i])
-      }
-    }
-
+  for (const {xs, ys, fill} of lineData) {
     const lineGenerator: any = line()
       .context(context)
       .x((i: any) => xScale(xs[i]))
@@ -65,7 +79,7 @@ const drawLines = ({
       .curve(CURVES[interpolation] || curveLinear)
 
     context.beginPath()
-    context.strokeStyle = fillScale(groupKey)
+    context.strokeStyle = fill
 
     lineGenerator(range(0, xs.length))
 
@@ -81,23 +95,26 @@ interface Props {
 }
 
 export const LineLayer: FunctionComponent<Props> = ({env, layerIndex}) => {
-  const canvas = useRef<HTMLCanvasElement>(null)
-
   const table = env.getTable(layerIndex)
   const fillScale = env.getScale(layerIndex, 'fill')
   const layer = env.config.layers[layerIndex] as LineLayerConfig
   const {interpolation, x: xColKey, y: yColKey} = layer
   const {xScale, yScale, innerWidth: width, innerHeight: height} = env
 
+  const lineData = useMemo(
+    // TODO: Simplify with Ramer-Douglas-Peucker as well
+    () => computeLineData(table, xColKey, yColKey, fillScale),
+    [table, xColKey, yColKey, fillScale]
+  )
+
+  const canvas = useRef<HTMLCanvasElement>(null)
+
   useLayoutEffect(() => {
     drawLines({
       canvas: canvas.current,
-      table,
-      xColKey,
-      yColKey,
+      lineData,
       xScale,
       yScale,
-      fillScale,
       interpolation,
       width,
       height,
