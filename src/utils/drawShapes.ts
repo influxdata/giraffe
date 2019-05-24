58de4a613f5e31a3db49fbe350c16689f58def11
Deniz Kusefoglu <deniz@influxdata.com>
2019-05-24 14:32:20 -0700

    Add tritip, stroked square, ex, and plus to shapes (#61)

--- src/utils/drawShapes.ts
+++ src/utils/drawShapes.ts
@@ -1,41 +1,112 @@
 export const drawCircle = (
   ctx: CanvasRenderingContext2D,
-  centerX: number,
-  centerY: number,
-  r?: number
+  x: number,
+  y: number,
+  radius: number = 2
 ) => {
-  const radius = r ? r : 2
-  const x = centerX - radius / 2
-  const y = centerY - radius / 2
-
-  const Zero = 0
-  const TwoPi = Math.PI * 2
-
-  ctx.arc(x, y, radius, Zero, TwoPi, true)
+  ctx.beginPath()
+  ctx.arc(x, y, radius, 0, Math.PI * 2, true)
+  ctx.fill()
 }
 
 export const drawSquare = (
   ctx: CanvasRenderingContext2D,
   centerX: number,
   centerY: number,
-  s?: number
+  size: number = 4
 ) => {
-  const size = s ? s : 6
   const x = centerX - size / 2
   const y = centerY - size / 2
 
-  ctx.fillRect(x, y, size, size)
+  ctx.lineWidth = 1
+
+  ctx.rect(x, y, size, size)
+  ctx.stroke()
 }
 
 export const drawTriangle = (
   ctx: CanvasRenderingContext2D,
-  x: number,
-  y: number,
-  s?: number
+  centerX: number,
+  centerY: number,
+  size: number = 3
 ) => {
-  const size = s ? s : 4
+  ctx.beginPath()
+  ctx.moveTo(centerX - size, centerY + size)
+  ctx.lineTo(centerX + size, centerY + size)
+  ctx.lineTo(centerX, centerY - size)
+  ctx.fill()
+}
 
-  ctx.moveTo(x - size, y + size)
-  ctx.lineTo(x + size, y + size)
-  ctx.lineTo(x, y - size)
+export const drawPlus = (
+  ctx: CanvasRenderingContext2D,
+  centerX: number,
+  centerY: number,
+  size: number = 6
+) => {
+  const mid = size / 2
+
+  ctx.lineWidth = 1
+
+  ctx.beginPath()
+  ctx.moveTo(centerX - mid, centerY)
+  ctx.lineTo(centerX + mid, centerY)
+  ctx.stroke()
+
+  ctx.beginPath()
+  ctx.moveTo(centerX, centerY - mid)
+  ctx.lineTo(centerX, centerY + mid)
+  ctx.stroke()
+}
+
+export const drawEx = (
+  ctx: CanvasRenderingContext2D,
+  centerX: number,
+  centerY: number,
+  size: number = 5
+) => {
+  const mid = size / 2
+
+  ctx.lineWidth = 1
+
+  ctx.beginPath()
+  ctx.moveTo(centerX - mid, centerY - mid)
+  ctx.lineTo(centerX + mid, centerY + mid)
+  ctx.closePath()
+  ctx.stroke()
+
+  ctx.beginPath()
+  ctx.moveTo(centerX - mid, centerY + mid)
+  ctx.lineTo(centerX + mid, centerY - mid)
+  ctx.closePath()
+  ctx.stroke()
+}
+
+export const drawTritip = (
+  ctx: CanvasRenderingContext2D,
+  centerX: number,
+  centerY: number,
+  size: number = 3
+) => {
+  const cos30 = 0.86602540378
+  const sin30 = 0.5
+
+  ctx.lineWidth = 1
+
+  ctx.beginPath()
+  ctx.moveTo(centerX, centerY)
+  ctx.lineTo(centerX + cos30 * size, centerY + sin30 * size)
+  ctx.closePath()
+  ctx.stroke()
+
+  ctx.beginPath()
+  ctx.moveTo(centerX, centerY)
+  ctx.lineTo(centerX - cos30 * size, centerY + sin30 * size)
+  ctx.closePath()
+  ctx.stroke()
+
+  ctx.beginPath()
+  ctx.moveTo(centerX, centerY)
+  ctx.lineTo(centerX, centerY - size)
+  ctx.closePath()
+  ctx.stroke()
 }
