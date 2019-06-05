7f19f34bdc9247419e7fc60a9851289616906e8e
Christopher Henn <chris@chrishenn.net>
2019-06-05 10:19:49 -0700

    Tweak simplification factor
    This gets rid of some artifacts from the downsampling I noticed.

--- src/utils/useChangedDepsLogger.ts
+++ src/utils/useChangedDepsLogger.ts
@@ -1,5 +1,26 @@
 import {useRef, MutableRefObject} from 'react'
 
+/*
+  This utility can be used to debug why a `useEffect` or `useMemo` hook is
+  firing.
+
+  For example, given the following hook:
+
+      useEffect(() => {
+        doTheThing()
+      }, [a, b, c])
+
+  We can place the `useChangeDepsLogger` immediately after:
+
+      useEffect(() => {
+        doTheThing()
+      }, [a, b, c])
+
+      useChangedDepsLogger({a, b, c})
+
+  If any of the dependencies `a`, `b`, or `c` changes in a render (causing the
+  effect to fire) then a message will be logged to the console.
+*/
 export const useChangedDepsLogger = deps => {
   const prevDeps: MutableRefObject<any> = useRef({})
 
