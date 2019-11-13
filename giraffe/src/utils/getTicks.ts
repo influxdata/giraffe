a4f545678c4d48758e200c3939ec1630800df66e
Ariel Salem <Asalem1@users.noreply.github.com>
2019-11-13 05:33:42 -0800

    fix: date weirdness for overlapping dates (#110)
    fix: formatting issue with overlapping dates
    fix(getticks): made PR suggestions to handle potentially fatal error

--- giraffe/src/utils/getTicks.ts
+++ giraffe/src/utils/getTicks.ts
@@ -3,8 +3,6 @@ import {ticks} from 'd3-array'
 
 import {Formatter, FormatterType} from '../types'
 
-const TICK_DENSITY = 0.5
-
 export const getTicks = (
   domain: number[],
   rangeLength: number,
@@ -13,7 +11,6 @@ export const getTicks = (
 ): number[] => {
   const sampleTick = formatter(domain[1])
   const numTicks = getNumTicks(sampleTick, rangeLength, charLength)
-
   switch (formatter._GIRAFFE_FORMATTER_TYPE) {
     case FormatterType.Time:
       return getTimeTicks(domain, rangeLength, numTicks)
@@ -29,9 +26,7 @@ const getNumTicks = (
   charLength: number
 ): number => {
   const sampleTickWidth = sampleTick.length * charLength
-  const numTicks = Math.round((length / sampleTickWidth) * TICK_DENSITY)
-
-  return Math.max(numTicks, 2)
+  return sampleTickWidth === 0 ? 0 : Math.round(length / sampleTickWidth)
 }
 
 const getTimeTicks = (
@@ -39,9 +34,16 @@ const getTimeTicks = (
   length: number,
   numTicks: number
 ): number[] => {
-  return scaleUtc()
+  const results = scaleUtc()
     .domain([d0, d1])
     .range([0, length])
     .ticks(numTicks)
     .map(d => d.getTime())
+  // added this to force D3 to use the numTicks since D3
+  // treats the tick params as suggestions:
+  // https://observablehq.com/@d3/scale-ticks
+  if (results.length > numTicks) {
+    return results.slice(0, numTicks)
+  }
+  return results
 }
