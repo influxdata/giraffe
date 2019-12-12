762588f1cf10d57bcfbd7219ad5969aa70fc00e4
Timmy Luong <TCL735@users.noreply.github.com>
2019-12-12 10:44:11 -0800

    fix: remove leading zero from 12-hour format

--- giraffe/src/utils/formatters.test.ts
+++ giraffe/src/utils/formatters.test.ts
@@ -15,7 +15,7 @@ describe('timeFormatter', () => {
     expect(utcFormatter(d)).toEqual('2019-01-01 00:00:00 UTC')
 
     // Defaults to 12 hour time when not UTC
-    expect(nonUTCFormatter(d)).toEqual('2018-12-31 04:00:00 PM PST')
+    expect(nonUTCFormatter(d)).toEqual('2018-12-31  4:00:00 PM PST')
   })
 
   test('uses AM/PM when given "a" in the format regardless of time zone or time format', () => {
@@ -44,7 +44,7 @@ describe('timeFormatter', () => {
     )
     expect(nonUTCFormatterWithFormat(d)).toEqual('2018-12-31 4:00:00 PM PST')
     expect(nonUTCFormatterWithFormatWithLowerH(d)).toEqual(
-      '2018-12-31 04:00:00 PM PST'
+      '2018-12-31  4:00:00 PM PST'
     )
   })
 
