cdaf53cc5bd85193424ae80cc975919cef186931
Timmy Luong <TCL735@users.noreply.github.com>
2020-01-07 10:36:22 -0800

    fix: use efficient spacing for small number of ticks and add explanations

--- giraffe/src/utils/__mocks__/getTextMetrics.ts
+++ giraffe/src/utils/__mocks__/getTextMetrics.ts
@@ -1,3 +1,12 @@
+/*
+  utils/getTextMetrics uses the DOM to measure the actual width and height of a string
+  Since Jest does not have access to the DOM, we will fix the width and height to be some arbitrary numbers,
+  so that our tests will run properly for consumers of getTextMetrics
+
+  This is a mock using a fake font that will always have
+  width equal to 1.2 times the length of the string
+  heigth equal to 12
+*/
 export const getTextMetrics = (...args) => ({
   width: Math.floor(args[1].length * 1.2),
   height: 12,
--- giraffe/src/utils/getTicks.ts
+++ giraffe/src/utils/getTicks.ts
@@ -18,27 +18,53 @@ const getNumTicks = (
   return sampleTickWidth === 0 ? 0 : Math.round(length / sampleTickWidth)
 }
 
+/*
+  Optimal spacing defined as:
+    I. when there are less than four ticks:
+      for each tick, spacing is one-quarter of the length of a tick
+      thusly: 0.25, 0.50, 0.75 of a tick's length as spacing for one, two, and three ticks respectively
+    II. when there are four or more ticks:
+      sum of all tick lengths is shorter than total available space by at least one tick's length
+    III. Based on rules I and II, we call d3 with the suggested number of ticks, and
+      allow d3 to determine the actual number of ticks and spacing
+*/
+const hasOptimalSpacing = (
+  rangeLength: number,
+  timeTickLength: number,
+  ticks: Date[]
+): boolean => {
+  const fractionalSpaceAsPadding = 0.25
+  const totalLength = ticks.length * timeTickLength
+  if (ticks.length < 4) {
+    const padding = timeTickLength * ticks.length * fractionalSpaceAsPadding
+    if (totalLength < rangeLength - padding) {
+      return true
+    }
+    return false
+  } else if (totalLength < rangeLength - timeTickLength) {
+    return true
+  }
+  return false
+}
+
 const getOptimalTimeTicks = (
   [d0, d1]: number[],
   rangeLength: number,
   timeTickLength: number
 ): Date[] => {
-  const maxUseableLength = rangeLength - timeTickLength
   const scaledTime = scaleUtc()
     .domain([d0, d1])
     .range([0, rangeLength])
-
   const maxNumTicks = Math.floor(rangeLength / timeTickLength)
-
   let optimalTicks = scaledTime.ticks(maxNumTicks)
-  let counter = 1
 
-  while (
+  for (
+    let counter = 1;
     counter < maxNumTicks &&
-    optimalTicks.length * timeTickLength > maxUseableLength
+    !hasOptimalSpacing(rangeLength, timeTickLength, optimalTicks);
+    counter += 1
   ) {
     optimalTicks = scaledTime.ticks(maxNumTicks - counter)
-    counter += 1
   }
   return optimalTicks
 }
