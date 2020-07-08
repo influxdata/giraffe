15e580d25704cbcf0f9fdbc7c0b3b9a24905f547
Rose Parker <reparker837@gmail.com>
2020-07-08 11:26:58 -0400

    deleted SERIES import comment

--- stories/src/index.stories.tsx
+++ stories/src/index.stories.tsx
@@ -2,7 +2,6 @@ import * as React from 'react'
 import {storiesOf} from '@storybook/react'
 import {withKnobs, number, select, boolean, text} from '@storybook/addon-knobs'
 import {CPUString} from './data/cpuString'
-// import {SERIES} from './data/mosaicDataSet'
 
 import {
   Config,
