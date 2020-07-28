df1ac377bc6ef4fe2527ddfce16d9f62a85bccb3
Rose Parker <reparker837@gmail.com>
2020-07-28 09:36:09 -0400

    Revert "potential solution to error with multiple data points for the same time range"
    This reverts commit 715a83a7ce497732932a8c7ec5f87ff8e8879677.

--- giraffe/package/src/utils/getJavaScriptTag.ts
+++
@@ -1,6 +0,0 @@
-export const getJavaScriptTag = (value: any): string => {
-  if (value == null) {
-    return value === undefined ? '[object Undefined]' : '[object Null]'
-  }
-  return Object.prototype.toString.call(value)
-}
