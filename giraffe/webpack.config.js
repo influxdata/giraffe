0cd8bbae9fded15be918886f6ec597d649054136
Bucky Schwarz <d.w.schwarz@gmail.com>
2020-11-02 12:05:54 -0800

    chore: use webpack for prod builds

---
+++ giraffe/webpack.config.js
@@ -0,0 +1,66 @@
+var path = require('path');
+var webpack = require('webpack');
+const pkg = require('./package.json')
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'production',
+  output: {
+    filename: 'index.js',
+    libraryTarget: 'umd',
+    library: pkg.name,
+    path: path.resolve(__dirname, 'dist'),
+    sourceMapFilename: '[file].map[query]',
+    libraryTarget: 'umd',
+  },
+  externals: {
+    react: 'react',
+    'react-dom': 'react-dom',
+    // lodash: 'lodash' TODO: should we externalize lodash?,
+  },
+  devtool: 'source-map',
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+    },
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.s?css$/i,
+        use: [
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[contenthash:10].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[contenthash:10].[ext]',
+            },
+          },
+        ],
+      },
+    ]
+  },
+};
