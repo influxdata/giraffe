6ac74eecdd6520340c4663abb104daf2a7fef633
Bucky Schwarz <d.w.schwarz@gmail.com>
2020-11-20 09:17:14 -0800

    test: add sourcemaps to stories so you can get a better sense of what broke

--- stories/.storybook/webpack.config.js
+++ stories/.storybook/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const PrettierPlugin = require('prettier-webpack-plugin')
 
 module.exports = ({config}) => {
+  config.devtool = 'eval-cheap-module-source-map'
   config.module.rules.push(
     {
       test: /\.woff(2)?(\?[a-z0-9#=&.]+)$/,
