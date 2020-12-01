2a50cd3bddaa382d79a9f466579af84573901444
Timmy Luong <TCL735@users.noreply.github.com>
2020-12-01 15:51:58 -0800

    chore: remove unused variable in index.stories

--- stories/src/index.stories.tsx
+++ stories/src/index.stories.tsx
@@ -2,7 +2,6 @@ import * as React from 'react'
 import {storiesOf} from '@storybook/react'
 import {withKnobs, number, select, boolean, text} from '@storybook/addon-knobs'
 import {CPUString} from './data/cpuString'
-import {CPU} from './data/cpu'
 
 import {
   Config,
