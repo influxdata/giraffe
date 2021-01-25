99937ae51e4e9641e776208f80cb2870c87ae01b
ChitlangeSahas <chitlangesahas@gmail.com>
2021-01-25 16:20:01 -0700

    table graph layer refactored.

--- stories/src/band.stories.tsx
+++ stories/src/band.stories.tsx
@@ -227,9 +227,9 @@ storiesOf('Band Chart', module)
           lineOpacity,
           hoverDimension,
           shadeOpacity,
-          // upperColumnName,
+          upperColumnName,
           mainColumnName,
-          // lowerColumnName,
+          lowerColumnName,
         },
       ],
     }
