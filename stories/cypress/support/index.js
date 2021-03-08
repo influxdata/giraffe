9df94dbd430eee1eb4cdf1143732a3b2c56b35e4
Sciator <39964450+Sciator@users.noreply.github.com>
2021-03-08 13:50:53 +0100

    test: skip error on dev snapshots

--- stories/cypress/support/index.js
+++ stories/cypress/support/index.js
@@ -19,7 +19,7 @@ import './commands'
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
-const resizeObserverLoopErrRe = /^ResizeObserver loop limit exceeded/
+const resizeObserverLoopErrRe = /> ResizeObserver loop limit exceeded/
 
 Cypress.on('uncaught:exception', err => {
   if (resizeObserverLoopErrRe.test(err.message)) {
