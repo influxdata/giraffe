54b5cd3aec166b22e9471eccc41e47c7b2a5bbff
ChitlangeSahas <chitlangesahas@gmail.com>
2021-03-25 12:10:06 -0700

    fix: move the tooltip out of the way for the annotation triangle pin.

--- giraffe/src/components/AnnotationTooltip.tsx
+++ giraffe/src/components/AnnotationTooltip.tsx
@@ -25,7 +25,7 @@ export const AnnotationTooltip: FunctionComponent<Props> = props => {
 
   const position = {
     x: dimension === 'x' ? startValue : width,
-    y: dimension === 'y' ? startValue : 0,
+    y: dimension === 'y' ? startValue : -10,
   } as TooltipPosition
 
   const clampedXOffset = Math.round(
