4ba1c3e4519a46a164b50d9380c8e439a1eecca4
Timmy Luong <TCL735@users.noreply.github.com>
2021-03-26 18:42:44 -0700

    feat: add fill column controls for static legend stories

--- stories/src/data/randomTable.ts
+++ stories/src/data/randomTable.ts
@@ -9,15 +9,38 @@ function getRandomNumber(max) {
   return Math.random() * Math.floor(max) - max / 2
 }
 
+export const makeColumnName = (length: number) => {
+  let result = ''
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+  const charactersLength = characters.length
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength))
+  }
+  return result
+}
+
+type ColumnsArg = number | Array<string>
+
+interface RandomFillColumns {
+  columnNames: Array<string>
+  columnValues: Array<Array<string>>
+}
+
 export const getRandomTable = memoizeOne(
   (
     maxValue: number,
     numberOfRecords: number = defaultNumberOfRecords,
-    recordsPerLine: number = defaultRecordsPerLine
+    recordsPerLine: number = defaultRecordsPerLine,
+    fillColumns?: ColumnsArg,
+    fillColumnNameLength?: number
   ) => {
     const timeColumn = []
     const valueColumn = []
     const cpuColumn = []
+    const randomFillColumns = {
+      columnNames: [],
+      columnValues: [],
+    } as RandomFillColumns
 
     for (let i = 0; i < numberOfRecords; i += 1) {
       valueColumn.push(getRandomNumber(maxValue))
@@ -25,9 +48,57 @@ export const getRandomTable = memoizeOne(
       timeColumn.push(now + (i % recordsPerLine) * 1000 * 60)
     }
 
-    return newTable(numberOfRecords)
+    if (!fillColumns) {
+      randomFillColumns.columnNames.push('cpu')
+      randomFillColumns.columnValues.push(cpuColumn)
+    }
+
+    if (typeof fillColumns === 'number') {
+      const nameLength =
+        typeof fillColumnNameLength === 'number' &&
+        fillColumnNameLength === fillColumnNameLength
+          ? fillColumnNameLength
+          : 4
+      for (let j = 0; j < fillColumns; j += 1) {
+        const randomFillColumn = []
+        const randomFillColumnName = makeColumnName(nameLength)
+        for (let k = 0; k < numberOfRecords; k += 1) {
+          randomFillColumn.push(
+            `${randomFillColumnName}${Math.floor(k / recordsPerLine)}`
+          )
+        }
+        randomFillColumns.columnNames.push(randomFillColumnName)
+        randomFillColumns.columnValues.push(randomFillColumn)
+      }
+    }
+
+    if (Array.isArray(fillColumns)) {
+      for (let j = 0; j < fillColumns.length; j += 1) {
+        const randomFillColumn = []
+        const fillColumnName = fillColumns[j]
+        for (let k = 0; k < numberOfRecords; k += 1) {
+          randomFillColumn.push(
+            `${fillColumnName}${Math.floor(k / recordsPerLine)}`
+          )
+        }
+        randomFillColumns.columnNames.push(fillColumnName)
+        randomFillColumns.columnValues.push(randomFillColumn)
+      }
+    }
+
+    let table = newTable(numberOfRecords)
       .addColumn('_time', 'dateTime:RFC3339', 'time', timeColumn)
       .addColumn('_value', 'system', 'number', valueColumn)
-      .addColumn('cpu', 'string', 'string', cpuColumn)
+
+    randomFillColumns.columnNames.forEach(
+      (name, index) =>
+        (table = table.addColumn(
+          name,
+          'string',
+          'string',
+          randomFillColumns.columnValues[index]
+        ))
+    )
+    return table
   }
 )
--- stories/src/staticLegend.stories.tsx
+++ stories/src/staticLegend.stories.tsx
@@ -12,6 +12,7 @@ import {
   xScaleKnob,
   yScaleKnob,
   fillKnob,
+  findStringColumns,
   colorSchemeKnob,
   legendFontKnob,
   tickFontKnob,
@@ -26,8 +27,17 @@ const maxValue = Math.random() * Math.floor(200)
 
 storiesOf('Static Legend', module)
   .addDecorator(withKnobs)
-  .add('Static Legend', () => {
-    const table = getRandomTable(maxValue)
+  .add('Legend with random fill column names', () => {
+    const lines = number('Number of graph lines', 4)
+    const fillColumnsCount = number('Number of fill columns', 5)
+    const fillColumnNameLength = number('Length of fill column names', 4)
+    const table = getRandomTable(
+      maxValue,
+      lines * 20,
+      20,
+      fillColumnsCount,
+      fillColumnNameLength
+    )
     const colors = colorSchemeKnob()
     const legendFont = legendFontKnob()
     const tickFont = tickFontKnob()
@@ -54,7 +64,7 @@ storiesOf('Static Legend', module)
       },
       'YYYY-MM-DD HH:mm:ss ZZ'
     )
-    const fill = fillKnob(table, ['cpu'])
+    const fill = fillKnob(table, findStringColumns(table))
     const position = select(
       'Line Position',
       {stacked: 'stacked', overlaid: 'overlaid'},
@@ -76,7 +86,7 @@ storiesOf('Static Legend', module)
       max: 1.0,
       step: 0.05,
     })
-    const legendOrientationThreshold = tooltipOrientationThresholdKnob()
+    const legendOrientationThreshold = tooltipOrientationThresholdKnob(20)
     const legendColorizeRows = tooltipColorizeRowsKnob()
 
     const staticLegendHeightRatio = number('Static Legend Height', 0.2, {
@@ -85,6 +95,113 @@ storiesOf('Static Legend', module)
       max: 1,
       step: 0.01,
     })
+    const config: Config = {
+      table,
+      valueFormatters: {
+        _time: timeFormatter({timeZone, format: timeFormat}),
+        _value: val =>
+          `${val.toFixed(2)}${
+            valueAxisLabel ? ` ${valueAxisLabel}` : valueAxisLabel
+          }`,
+      },
+      xScale,
+      yScale,
+      legendFont,
+      tickFont,
+      showAxes,
+      legendOpacity,
+      legendOrientationThreshold,
+      legendColorizeRows,
+      staticLegend: {
+        heightRatio: staticLegendHeightRatio,
+        border: '1px solid orange',
+        fontBrightColor: 'yellow',
+      },
+      layers: [
+        {
+          type: 'line',
+          x,
+          y,
+          fill,
+          position,
+          interpolation,
+          colors,
+          lineWidth,
+          hoverDimension,
+          shadeBelow,
+          shadeBelowOpacity,
+        },
+      ],
+    }
+
+    return (
+      <PlotContainer>
+        <Plot config={config} />
+      </PlotContainer>
+    )
+  })
+  .add('Legend with random custom fill column names', () => {
+    const lines = number('Number of graph lines', 4)
+    const fillColumnsText = text('fillColumns', 'cluster,host,machine,cpu')
+    const fillColumnNames = fillColumnsText.split(',')
+    const staticLegendHeightRatio = number('Static Legend Height', 0.2, {
+      range: true,
+      min: 0,
+      max: 1,
+      step: 0.01,
+    })
+    const table = getRandomTable(maxValue, 20 * lines, 20, fillColumnNames)
+    const colors = colorSchemeKnob()
+    const legendFont = legendFontKnob()
+    const tickFont = tickFontKnob()
+    const x = xKnob(table)
+    const y = yKnob(table)
+    const valueAxisLabel = text('Value Axis Label', 'foo')
+    const xScale = xScaleKnob()
+    const yScale = yScaleKnob()
+    const timeZone = timeZoneKnob()
+    const timeFormat = select(
+      'Time Format',
+      {
+        'DD/MM/YYYY HH:mm:ss.sss': 'DD/MM/YYYY HH:mm:ss.sss',
+        'MM/DD/YYYY HH:mm:ss.sss': 'MM/DD/YYYY HH:mm:ss.sss',
+        'YYYY/MM/DD HH:mm:ss': 'YYYY/MM/DD HH:mm:ss',
+        'YYYY-MM-DD HH:mm:ss ZZ': 'YYYY-MM-DD HH:mm:ss ZZ',
+        'hh:mm a': 'hh:mm a',
+        'HH:mm': 'HH:mm',
+        'HH:mm:ss': 'HH:mm:ss',
+        'HH:mm:ss ZZ': 'HH:mm:ss ZZ',
+        'HH:mm:ss.sss': 'HH:mm:ss.sss',
+        'MMMM D, YYYY HH:mm:ss': 'MMMM D, YYYY HH:mm:ss',
+        'dddd, MMMM D, YYYY HH:mm:ss': 'dddd, MMMM D, YYYY HH:mm:ss',
+      },
+      'YYYY-MM-DD HH:mm:ss ZZ'
+    )
+    const fill = fillKnob(table, findStringColumns(table))
+    const position = select(
+      'Line Position',
+      {stacked: 'stacked', overlaid: 'overlaid'},
+      'overlaid'
+    )
+    const interpolation = interpolationKnob()
+    const showAxes = showAxesKnob()
+    const lineWidth = number('Line Width', 1)
+    const shadeBelow = boolean('Shade Area', false)
+    const shadeBelowOpacity = number('Area Opacity', 0.1)
+    const hoverDimension = select(
+      'Hover Dimension',
+      {auto: 'auto', x: 'x', y: 'y', xy: 'xy'},
+      'auto'
+    )
+    const legendOpacity = number('Legend Opacity', 1.0, {
+      range: true,
+      min: 0,
+      max: 1.0,
+      step: 0.05,
+    })
+    const legendOrientationThreshold = tooltipOrientationThresholdKnob(20)
+    const legendColorizeRows = tooltipColorizeRowsKnob()
+
     const config: Config = {
       table,
       valueFormatters: {
