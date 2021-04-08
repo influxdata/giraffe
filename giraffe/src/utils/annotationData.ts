50b7c0eee36ed2959628987af72fb622f764de05
ChitlangeSahas <chitlangesahas@gmail.com>
2021-04-08 15:57:50 -0700

    chore: tweak comment

--- giraffe/src/utils/annotationData.ts
+++ giraffe/src/utils/annotationData.ts
@@ -49,7 +49,7 @@ const distanceToMousePointer = (
       // hover position is in the range, effective distance is 0
       return 0
     } else {
-      // hoverPosition has a distance that is shortest of:
+      // distance is the shortest of:
       // the distance between hoverPosition and annotation.startValue,
       // and the distance between hoverPosition and annotation.stopValue
       const distanceToStartLine = Math.abs(
