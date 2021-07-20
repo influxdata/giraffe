1dec47b68b8f94fd69756a93eb6e87a6832b72f7
Merrick Tian <mtian725@gmail.com>
2021-07-20 11:53:05 -0400

    fix: added suggestions

--- giraffe/src/types/index.ts
+++ giraffe/src/types/index.ts
@@ -456,7 +456,7 @@ export interface ScatterLayerConfig {
 }
 
 export interface SimpleTableLayerConfig {
-  type: 'simple table' // do not refactor or restrict to LayerTypes.Mosaic
+  type: 'simple table' // do not refactor or restrict to LayerTypes.SimpleTable
   showAll: boolean
   fluxResponse?: string
 }
@@ -923,45 +923,4 @@ export interface AnnotationMark {
   dimension: AnnotationDimension
   pin: AnnotationPinType
   id?: string
-}
-
-export enum ComponentColor {
-  Default = 'default',
-  Primary = 'primary',
-  Secondary = 'secondary',
-  Tertiary = 'tertiary',
-  Success = 'success',
-  Warning = 'warning',
-  Danger = 'danger',
-}
-
-export enum Alignment {
-  Left = 'left',
-  Center = 'center',
-  Right = 'right',
-}
-
-export enum VerticalAlignment {
-  Top = 'top',
-  Middle = 'middle',
-  Bottom = 'bottom',
-}
-
-export enum ButtonShape {
-  Default = 'none',
-  Square = 'square',
-  StretchToFit = 'stretch',
-}
-
-export enum ButtonType {
-  Button = 'button',
-  Submit = 'submit',
-}
-
-export enum BorderType {
-  None = 'none',
-  Horizontal = 'horizontal',
-  Vertical = 'vertical',
-  Both = 'both',
-  All = 'all',
 }
