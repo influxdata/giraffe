4053c75c559a4b282a805d89552ad68291481713
ChitlangeSahas <chitlangesahas@gmail.com>
2021-09-08 10:50:16 -0700

    chore: bump giraffe version

--- stories/src/data/tableGraph.ts
+++ stories/src/data/tableGraph.ts
@@ -1,5 +1,5 @@
 export const tableCSV = `#group,false,false,true,true,false,false,true,true,true,true
-#datatype,string,long,dateTime:RFC3339,dateTime:RFC3339,dateTime:RFC3339,string,string,string,string,string
+#datatype,string,long,dateTime:RFC3339,dateTime:RFC3339,dateTime:RFC3339,double,string,string,string,string
 #default,_result,,,,,,,,,
 ,result,table,_start,_stop,_time,_value,_field,_measurement,cpu,host
 ,,0,2020-07-01T21:45:43.0968Z,2020-07-01T21:50:43.0968Z,2020-07-01T21:45:50Z,2.2,usage_system,cpu,cpu1,MBP15-TLUONG.local
