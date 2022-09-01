f5e24767790bdc76b906e0b0748a1d8348b4419d
Timmy Luong <TCL735@users.noreply.github.com>
2022-09-01 12:46:40 -0700

    fix: update Band to avoid using the Douglas Peucker algorithm (#800)

--- giraffe/src/utils/lineData.test.ts
+++ giraffe/src/utils/lineData.test.ts
@@ -1,12 +1,17 @@
 import {DomainLabel} from '../types'
-import {getDomainDataFromLines} from './lineData'
+import {
+  getDomainDataFromLines,
+  simplifyBandData,
+  simplifyLineData,
+} from './lineData'
+import {getLinearScale} from './getLinearScale'
 
 describe('lineData', () => {
   describe('get domain data from line data', () => {
     const lineData = {
       0: {
-        xs: [],
-        ys: [],
+        xs: [] as any,
+        ys: [] as any,
         fill: 'some color',
       },
     }
@@ -43,6 +48,81 @@ describe('lineData', () => {
           DomainLabel.Y
         )
       ).toEqual([1, 2, 3, 4, 5, 6, 7, 8])
+    })
+  })
+
+  describe('simplifyBandData', () => {
+    const NOW = new Date().getTime()
+    const TWENTY_FOUR_HOURS_FROM_NOW = NOW + 1000 * 60 * 60 * 24
+    const xScale = getLinearScale(NOW, TWENTY_FOUR_HOURS_FROM_NOW, 0, 1200)
+
+    const yScale = getLinearScale(0, 100, 800, 0)
+
+    it('returns the same number of points that it receives for each line', () => {
+      const lineData = {
+        0: {
+          fill: '#31C0F6',
+          xs: [
+            NOW + 1000 * 60 * 60 * 1,
+            NOW + 1000 * 60 * 60 * 2,
+            NOW + 1000 * 60 * 60 * 3,
+            NOW + 1000 * 60 * 60 * 4,
+            NOW + 1000 * 60 * 60 * 5,
+            NOW + 1000 * 60 * 60 * 6,
+            NOW + 1000 * 60 * 60 * 7,
+            NOW + 1000 * 60 * 60 * 8,
+          ],
+          ys: [77, 77, 77, 90, 90, 77, 77, 77],
+        },
+        1: {
+          fill: '#31C0F6',
+          xs: [
+            NOW + 1000 * 60 * 60 * 1,
+            NOW + 1000 * 60 * 60 * 2,
+            NOW + 1000 * 60 * 60 * 3,
+            NOW + 1000 * 60 * 60 * 4,
+            NOW + 1000 * 60 * 60 * 5,
+            NOW + 1000 * 60 * 60 * 6,
+            NOW + 1000 * 60 * 60 * 7,
+            NOW + 1000 * 60 * 60 * 8,
+          ],
+          ys: [25, 40, 32, 22, 58, 33, 52, 66],
+        },
+        2: {
+          fill: '#31C0F6',
+          xs: [
+            NOW + 1000 * 60 * 60 * 1,
+            NOW + 1000 * 60 * 60 * 2,
+            NOW + 1000 * 60 * 60 * 3,
+            NOW + 1000 * 60 * 60 * 4,
+            NOW + 1000 * 60 * 60 * 5,
+            NOW + 1000 * 60 * 60 * 6,
+            NOW + 1000 * 60 * 60 * 7,
+            NOW + 1000 * 60 * 60 * 8,
+          ],
+          ys: [1, 1, 1, 2, 1, 1, 1, 1],
+        },
+      }
+
+      const truncatedResult = simplifyLineData(lineData, xScale, yScale)
+      expect(truncatedResult[0].xs.length).not.toEqual(lineData[0].xs.length)
+      expect(truncatedResult[2].xs.length).not.toEqual(lineData[2].xs.length)
+      expect(truncatedResult[0].xs.length).not.toEqual(
+        truncatedResult[1].xs.length
+      )
+      expect(truncatedResult[1].xs.length).not.toEqual(
+        truncatedResult[2].xs.length
+      )
+
+      const result = simplifyBandData(lineData, xScale, yScale)
+      expect(result[0].xs.length).toEqual(lineData[0].xs.length)
+      expect(result[0].ys.length).toEqual(lineData[0].ys.length)
+
+      expect(result[1].xs.length).toEqual(lineData[1].xs.length)
+      expect(result[1].ys.length).toEqual(lineData[1].ys.length)
+
+      expect(result[2].xs.length).toEqual(lineData[2].xs.length)
+      expect(result[2].ys.length).toEqual(lineData[2].ys.length)
     })
   })
 })
