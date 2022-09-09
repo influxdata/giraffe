e3639595c1e93b7b35c990a07dc856972ea134d2
Timmy Luong <TCL735@users.noreply.github.com>
2022-09-09 09:49:41 -0700

    test: add a story and large data set for Simple Table

--- stories/src/simpleTable.stories.tsx
+++ stories/src/simpleTable.stories.tsx
@@ -4,6 +4,7 @@ import {boolean, text, withKnobs} from '@storybook/addon-knobs'
 import {PlotContainer} from './helpers'
 import {Config, Plot, fromFlux} from '../../giraffe/src'
 import {tableCSV, nonNumbersInNumbersColumn} from './data/tableGraph'
+import {largeDataSet} from './data/largeDataSet'
 
 storiesOf('Simple Table', module)
   .addDecorator(withKnobs)
@@ -37,6 +38,29 @@ storiesOf('Simple Table', module)
 
     const config: Config = {
       fluxResponse: nonNumbersInNumbersColumn,
+      layers: [
+        {
+          type: 'simple table',
+          showAll: showAll,
+        },
+      ],
+    }
+
+    return (
+      // Simple Table needs a black background by default,
+      //   override Storybook's dark grey
+      <PlotContainer style={{backgroundColor}}>
+        <Plot config={config} />
+      </PlotContainer>
+    )
+  })
+  .add('Very large data set', () => {
+    const backgroundColor = text('Background contrast color:', 'black')
+
+    const showAll = boolean('showAll', false)
+
+    const config: Config = {
+      fluxResponse: largeDataSet,
       layers: [
         {
           type: 'simple table',
